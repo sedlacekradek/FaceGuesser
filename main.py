6e90b794efccb287993cb78da942396b1f1da4d5
sedlacekradek <113298261+sedlacekradek@users.noreply.github.com>
2022-10-15 15:45:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -72,10 +72,10 @@ def tutorial():
 
 
 # - - - TO TEST LOCALLY - - -
-# if __name__ == "__main__":
-#     from waitress import serve
+if __name__ == "__main__":
+    from waitress import serve
 #     print("localhost:8080")
-#     serve(app, host='0.0.0.0', port=8080)
+    serve(app)
 
 
 # if __name__ == "__main__":
